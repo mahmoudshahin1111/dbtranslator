3546efb90817ce571be7359954a59dcc1d8e5527
Mahmoud shahin <mahmoudshahin1111@gmail.com>
2019-10-15 15:53:09 +0200

    Update trans_routes.php

--- src/routes/trans_routes.php
+++ src/routes/trans_routes.php
@@ -1,6 +1,5 @@
 <?php
 
 Route::get('/', 'LiteMs\Translator\TranslationController@index');
-Route::post('/', 'TranslationController@store');
-
+Route::post('/', 'LiteMs\Translator\TranslationController@store');
 Route::get('/translation/{trans_id}/{lang}', 'LiteMs\Translator\TranslationController@show');
